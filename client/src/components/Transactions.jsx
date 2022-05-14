bb497367e920677988a79d1629a24ace2e22776d
Sergio Valadez <sergioval@gmail.com>
2022-05-14 10:56:23 -0500

    initial commit

---
+++ client/src/components/Transactions.jsx
@@ -0,0 +1,23 @@
+// import React, { useContext } from 'react';
+
+// import { TransactionContext } from '../context/TransactionContext';
+
+// import dummyData from '../utils/dummyData';
+// const Transactions= () => {
+//   const { currentAccount } = useContext(TransactionContext);
+//   return (
+//     <div className='flex w-full justify-center items-center 2xl:px20 gradient-bg-transactions'>
+//       <div className='flex flex-col md_p-12 py-12 px-4'>
+//         {currentAccount ? (
+//           <h3 className='text-white text-3xl my-2 text-center'>Latest Transactions</h3>
+//         ) : (
+//           <h3 className='text-white text-3xl my-2 text-center'>Connect your wallet yo see the latest transactions</h3>)}
+      
+      
+//     </div>
+//   </div>
+//   );
+// }
+
+//export default Transactions;
+//Transactions
